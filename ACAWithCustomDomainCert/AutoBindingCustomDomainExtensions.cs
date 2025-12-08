73ab7f03a550f7fda880402249064c37e6b20036
Tiago Alves Macambira <tiagoa@microsoft.com>
2025-12-08 18:57:48 +0000

    Remove AzureDnsOwnershipVerificationResource.
    It wasn't being used in the last iteration, better remove it and simplify the code.

--- ACAWithCustomDomainCert/AutoBindingCustomDomainExtensions.cs
+++ ACAWithCustomDomainCert/AutoBindingCustomDomainExtensions.cs
@@ -1,13 +1,19 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Net;
 using Aspire.Hosting.Azure;
 using Aspire.Hosting.Azure.AppContainers;
 using Azure.Provisioning;
 using Azure.Provisioning.AppContainers;
+using Azure.Provisioning.Dns;
 using Azure.Provisioning.Expressions;
 
+
 namespace ACAWithCustomDomainCert;
+
+#pragma warning disable AZPROVISION001 // DNS Provisioning is still in beta
+
 
 public static class AutoBindingCustomDomainExtensions
 {
@@ -127,5 +133,89 @@ public static class AutoBindingCustomDomainExtensions
         autoBindManagedCertificate.DependsOn.Add(app);
         autoBindManagedCertificate.DependsOn.Add(dnsRecordA); // The A record is required for cert binding. It is checked during cert creation..
         containerAppInfrastructure.Add(autoBindManagedCertificate);
+    }
+}
+
+
+public static class AzureDnsOwnershipVerificationResourceExtension
+{
+
+
+    public static (DnsTxtRecord, DnsARecord) ConfigureInfrastructure(
+        string hostname,
+        string dnsDomain,
+        BicepValue<string> subscriptionCustomDomainVerificationId,
+        BicepValue<IPAddress> containerAppEnvironmentStaticIP,
+        Infrastructure infrastructure)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(hostname, nameof(hostname));
+        ArgumentException.ThrowIfNullOrEmpty(dnsDomain, nameof(dnsDomain));
+        ArgumentNullException.ThrowIfNull(subscriptionCustomDomainVerificationId);
+        ArgumentNullException.ThrowIfNull(containerAppEnvironmentStaticIP);
+        ArgumentNullException.ThrowIfNull(infrastructure);
+
+        // Notice: `dnsZone` is assumed to be an existing resource not created under Aspire so we
+        // cannot use AzureProvisioningResource.CreateExistingOrNewProvisionableResource here.
+
+        // We are going to create a few DNS records to prove we own the domain
+        // and then create the custom domain binding in the Container App.
+        // We need access to a DnsZone resource to host those records.
+        DnsZone dnsZone = DnsZone.FromExisting(nameof(dnsZone));
+        dnsZone.Name = dnsDomain;
+        infrastructure.Add(dnsZone);
+
+        // TXT 'asuid' record is required and checked during containerApp deployment (due to configuration.ingress.customDomains)
+        DnsTxtRecord dnsAsuidTxtRecord = new(nameof(dnsAsuidTxtRecord))
+        {
+            Name = $"asuid.{hostname}", // Remember: not arbitrary, must be 'asuid.<your-app-name>'
+            TtlInSeconds = 3600,
+            Parent = dnsZone,
+            TxtRecords = {
+                    new DnsTxtRecordInfo()
+                    {
+                        Values = [ subscriptionCustomDomainVerificationId ]
+                    }
+                }
+        };
+        infrastructure.Add(dnsAsuidTxtRecord);
+
+        // A record pointing to the CAE environment - required by the certificate auto-binding logic during cert creation and binding
+        DnsARecord dnsRecordA = new(nameof(dnsRecordA))
+        {
+            Name = hostname, // Remember: not arbitrary, must be '<your-app-name>'
+            TtlInSeconds = 3600,
+            Parent = dnsZone,
+            ARecords =
+                {
+                    new DnsARecordInfo()
+                    {
+                        Ipv4Address = containerAppEnvironmentStaticIP
+                    }
+                },
+        };
+        infrastructure.Add(dnsRecordA);
+
+        // Add outputs 
+        infrastructure.Add(new ProvisioningOutput("dnsZoneName", typeof(string)) { Value = dnsZone.Name });
+        infrastructure.Add(new ProvisioningOutput("hostname", typeof(string)) { Value = dnsRecordA.Name });
+        infrastructure.Add(new ProvisioningOutput("fqdn", typeof(string)) { Value = $"{hostname}.{dnsDomain}" });
+
+        return (dnsAsuidTxtRecord, dnsRecordA);
+    }
+
+    public static void GetVerificationIdAndStaticIP(
+        IResourceBuilder<AzureContainerAppEnvironmentResource> cae,
+        AzureResourceInfrastructure infrastructure,
+        out BicepValue<string> subscriptionCustomDomainVerificationId,
+        out BicepValue<IPAddress> containerAppEnvironmentStaticIP)
+    {
+        // CAE verificationId and IP Address
+        //
+        // Think of AzureResourceInfrastructure as a Bicep module. We are defining
+        // a reference to an existing Container App Environment resource in this "scope"
+        // so we have a resource we can refer to.
+        ContainerAppManagedEnvironment containerAppEnvironment = (ContainerAppManagedEnvironment)cae.Resource.AddAsExistingResource(infrastructure);
+        subscriptionCustomDomainVerificationId = containerAppEnvironment.CustomDomainConfiguration.CustomDomainVerificationId;
+        containerAppEnvironmentStaticIP = containerAppEnvironment.StaticIP;
     }
 }
